f3aded982cf4172220c97ac3eb90874574153de4
aanand93 <alex329@mac.com>
2021-02-21 11:48:10 -0500

    Create psudo code to set up what I want todo for the flip card function

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+
+I want to create a function that flips each card over to see the other side.
+
+-I need to call the class of all the cards
+-create a event listener that listens for a clic
+-then triggers a function that will actually flip the cards
+
+
+*/
