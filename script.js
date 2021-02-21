5a56999894539a57cd9139aaf2298d04a932f008
aanand93 <alex329@mac.com>
2021-02-21 12:53:29 -0500

    Created styling to 3d flip the card and show the other image on the back of the card

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ const cards = document.querySelectorAll('.flashcard');
 // console.log(cards);
 
 function flipCard() {
-	console.log('Card has been clicked');
-	console.log(this);
+	// console.log('Card has been clicked');
+	// console.log(this);
+	this.classList.toggle('flip');
+	// ^ changes the class from 'flashcard' to 'flashcard flip'
 }
 
 cards.forEach((card) => card.addEventListener('click', flipCard));
